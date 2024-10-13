0fc3a6f9bbc4e0a9a084aa48875ed4f1b4cc9808
zarina-magkeeva <salmazarina25@gmail.com>
2024-10-13 19:01:38 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# 1st program
+print(9**0.5*5)
+
+# 2nd program
+print(9.99>9 and 1000!=1000.1)
+
+# 3rd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+
+# 4th program
+print(type(float('123.456')))
+print(int(123.456*10%10))
+
+
